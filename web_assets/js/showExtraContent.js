d8d1a6a4cfa1b6123c96e0b7833ea047ea382823
= <sher.shah010@gmail.com>
2019-11-28 23:20:01 -0800

    updated

--- web_assets/js/showExtraContent.js
+++ web_assets/js/showExtraContent.js
@@ -1,5 +1,4 @@
 function toggleShow(ev, id) {
-  console.log(ev);
   if (ev.srcElement.className.includes('loadbox') || ev.srcElement.offsetParent.className.includes('loadbox')) {
     document.getElementById(id).classList.toggle('show-extra');
   }
