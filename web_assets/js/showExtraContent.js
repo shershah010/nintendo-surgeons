4dcc52b0bb09b806fd710b9a441fbd52909f7e12
= <sher.shah010@gmail.com>
2019-12-01 18:23:43 -0800

    updated team page

--- web_assets/js/showExtraContent.js
+++ web_assets/js/showExtraContent.js
@@ -1,5 +1,3 @@
 function toggleShow(ev, id) {
-  if (ev.srcElement.className.includes('loadbox') || ev.srcElement.offsetParent.className.includes('loadbox')) {
-    document.getElementById(id).classList.toggle('show-extra');
-  }
+  document.getElementById(id).classList.toggle('show-extra');
 }
